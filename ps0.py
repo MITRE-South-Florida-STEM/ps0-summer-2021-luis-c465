f3bfae299f6992074f7123b70fe9613f2d9aefab
Luis Canada <80722367+luis-c465@users.noreply.github.com>
2021-07-06 22:08:34 +0000

    Answer question 1

--- ps0.py
+++ ps0.py
@@ -1,10 +1,17 @@
-# 1. Ask the user to enter a number "x"
+import numpy as np
 
+# 1. Ask the user to enter a number "x"
+input_x = input("Enter number x: ")
 
 # 2. Ask the user to enter a number "y"
+input_y = input("Enter number y: ")
 
+num_x = int(input_x)
+num_y = int(input_y)
 
 # 3. Prints out the number "x" raised to the power "y"
-
+print(f"x**y = { num_x ** num_y }")
 
 # 4. Prints out the log (base 2) of "x"
+# FIXME Will crash if x is a non positive number
+print(f"log(x) = { round( np.log2( num_x ) ) }")
